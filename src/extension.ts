ea8ec77a453de855e25976d335499f28787d0562
Andrew Branch <andrew@wheream.io>
2020-05-29 10:56:37 -0700

    Add new command for TSSERVER_DEBUG_BRK

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,29 @@ import { findFreePort } from "./lib/findFreePort";
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand("io.wheream.vscode-tsserver-debug.restart-with-debugging", restartWithDebuggingEnabled)
+		vscode.commands.registerCommand("io.wheream.vscode-tsserver-debug.restart-with-debugging", restartWithDebuggingEnabled),
+		vscode.commands.registerCommand("io.wheream.vscode-tsserver-debug.restart-with-debugging-brk", () => restartWithDebuggingEnabled(true)),
 	);
 }
 
-async function restartWithDebuggingEnabled() {
+async function restartWithDebuggingEnabled(brk?: boolean) {
 	const config = vscode.workspace.getConfiguration("tsserver-debug");
 	const timeout = Math.max(1000, config.get("discoveryTimeout", 3000));
 	const debugPort = config.get("debugPort", 9559);
 	const port = await findFreePort(debugPort, 1000, timeout);
 	if (port) {
-		process.env.TSS_DEBUG = String(port);
+		const varName = brk ? "TSS_DEBUG_BRK" : "TSS_DEBUG";
+		const saveValue = process.env[varName];
+		process.env[varName] = String(port);
 		try {
 			await vscode.commands.executeCommand("typescript.restartTsServer");
-			await vscode.window.showInformationMessage(`TS Server listening on port ${port}.`);
+			return vscode.window.showInformationMessage(`TS Server listening on port ${port}.`);
 		} catch {
-			await vscode.window.showWarningMessage(`TS Server was not running. If this window starts TS Server later, it will start listening on port ${port}.`);
+			return vscode.window.showWarningMessage(`TS Server was not running. If this window starts TS Server later, it will start listening on port ${port}.`);
+		} finally {
+			process.env[varName] = saveValue;
 		}
-	}
-	else {
+	} else {
 		await vscode.window.showErrorMessage(`Timeout elapsed prior to discovering an open port to use for debugging.`);
 	}
 }
