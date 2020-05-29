7c3e7e167c4e5569a8427c0c4a23c2f14c4eb953
Andrew Branch <andrew@wheream.io>
2020-05-29 10:56:37 -0700

    Add new command for TSSERVER_DEBUG_BRK

--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,23 @@ import { findFreePort } from "./lib/findFreePort";
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand("io.wheream.vscode-tsserver-debug.restart-with-debugging", restartWithDebuggingEnabled)
+		vscode.commands.registerCommand("io.wheream.vscode-tsserver-debug.restart-with-debugging", restartWithDebuggingEnabled),
+		vscode.commands.registerCommand("io.wheream.vscode-tsserver-debug.restart-with-debugging-brk", () => restartWithDebuggingEnabled(true)),
 	);
 }
 
-async function restartWithDebuggingEnabled() {
+async function restartWithDebuggingEnabled(brk?: boolean) {
 	const port = String(await findFreePort(9559, 1000, 1000));
-	process.env.TSS_DEBUG = port;
+	const varName = brk ? 'TSS_DEBUG_BRK' : 'TSS_DEBUG';
+	const saveValue = process.env[varName];
+	process.env[varName] = port;
 	try {
 		await vscode.commands.executeCommand("typescript.restartTsServer");
 		return vscode.window.showInformationMessage(`TS Server listening on port ${port}.`);
 	} catch {
 		return vscode.window.showWarningMessage(`TS Server was not running. If this window starts TS Server later, it will start listening on port ${port}.`);
+	} finally {
+		process.env[varName] = saveValue;
 	}
 }
 
